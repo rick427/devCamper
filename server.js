5b5c8ad4f906c7abb2d15194bb8c34f3da0178ef
rick427 <rchardnjoku@yahoo.com>
2019-10-26 15:04:25 +0100

    building custom console middlware

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 require('dotenv').config({path: './config/config.env'});
+const morgan = require('morgan');
 
 //Route files
 const bootcamps = require('./routes/bootcamps');
@@ -7,6 +8,9 @@ const bootcamps = require('./routes/bootcamps');
 const app = express();
 
 app.use(express.json());
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
 
 //Mount Routers
 app.use('/api/v1/bootcamps', bootcamps);
