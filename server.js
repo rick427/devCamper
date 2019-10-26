66816d10e007e9b073c8f5baa73cf20a9206261b
rick427 <rchardnjoku@yahoo.com>
2019-10-26 12:22:15 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+require('dotenv').config({path: './config.env'});
+
+const app = express();
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+app.listen(port, console.log(`Server is running in ${process.env.NODE_ENV} on port ${port}`))
