448a1975f3042013ea8c592122e4f86910675892
rick427 <rchardnjoku@yahoo.com>
2019-10-28 13:26:19 +0100

    finishing up mongoose error handling

--- middleware/error.js
+++ middleware/error.js
@@ -5,14 +5,24 @@ const errorHandler = (err, req, res, next) => {
   error.message = err.message
 
   //Log into the console
-  console.log(err.stack.red);
+  console.log(err);
 
   //Mongoose bad ObjectId
-  console.log(err.name);
-
   if(err.name === 'CastError'){
     const message = `Bootcamp not found with the id of ${err.value}`;
     error = new ErrorResponse(message, 404);
+  }
+
+  //Mongoose duplicate keys
+  if(err.code === 11000){
+      const message = 'Duplicate field value entered';
+      error = new ErrorResponse(message, 400);
+  }
+
+  //Mongoose validation errors
+  if(err.name === 'ValidationError'){
+      const message = Object.values(err.errors).map(val => val.message);
+      error = new ErrorResponse(message, 400);
   }
 
   res.status(error.statusCode || 500).json({
