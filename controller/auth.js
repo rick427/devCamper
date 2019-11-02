3be45a6aedb759ed28028d6e90bdd37a7ab35c97
rick427 <rchardnjoku@yahoo.com>
2019-11-02 14:35:15 +0100

    register a user and encrypt the password

--- controller/auth.js
+++ controller/auth.js
@@ -6,5 +6,17 @@ const asyncHandler = require('../middleware/async');
 // @route    POST /api/v1/auth/register
 // @access   PUBLIC
 exports.register = asyncHandler(async (req, res, next) => {
-    res.status(200).json({status: 'success'})
+    const {name, email, password, role} = req.body;
+    //create user
+    const user = await User.create({
+        name,
+        email,
+        password,
+        role
+    });
+
+    res.status(200).json({
+        status: 'success',
+        data: user
+    })
 })
--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -32,6 +33,13 @@ const UserSchema = new mongoose.Schema({
         default: Date.now
     }
 });
+
+// Encrypt password using bcrypt
+UserSchema.pre('save', async function(next){
+ const salt = await bcrypt.genSalt(10);
+ this.password = await bcrypt.hash(this.password, salt);
+ next();
+})
 
 module.exports = mongoose.model('User', UserSchema);
 
