cc00759323ecb951b4effd3d2fae8c51ab49b168
rick427 <rchardnjoku@yahoo.com>
2019-11-02 12:24:18 +0100

    add a course

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -15,6 +15,7 @@ const router = express.Router();
 // Re-route into other resource routers
 router.use('/:bootcampId/courses', courseRouter);
 
+
 router.route('/radius/:zipcode/:distance')
   .get(getBootcampsInRadius);
 
--- routes/courses.js
+++ routes/courses.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const {
- getCourses
+ getCourses,
+ getCourse,
+ addCourse
 } = require('../controller/courses');
 
 const router = express.Router({mergeParams: true});
 
-router.route('/').get(getCourses)
+router.route('/')
+  .get(getCourses)
+  .post(addCourse)
+router.route('/:id').get(getCourse);
+
 
 module.exports = router;
